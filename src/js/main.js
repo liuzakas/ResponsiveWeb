50de0305d8dbf9a2ad83281dd04fffdafcc121ea
Zakasliu <liuzihaovvv@qq.com>
2017-10-18 00:07:53 +0800

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by liu_zh on 2017/7/28.
+ */
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+})
